309b6f433a929961e05a1ec77db644df55dfb2f3
vellimir <kkazancov@gmail.com>
2026-09-13 20:33:21 +0400

    install libs

---
+++ app/authorizate/security.py
@@ -0,0 +1,18 @@
+from pwdlib import PasswordHash
+
+
+password_hasher = PasswordHash.recommended()
+
+
+def hash_password(password: str) -> str:
+    return password_hasher.hash(password)
+
+
+def verify_password(
+    password: str,
+    password_hash: str,
+) -> bool:
+    return password_hasher.verify(
+        password,
+        password_hash,
+    )
