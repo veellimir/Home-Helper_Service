b4f7082c097def37406290dbf94229c2c784a400
vellimir <kkazancov@gmail.com>
2026-09-18 17:08:28 +0400

    added bookings router

--- api/api_v1/bookings_router.py
+++ api/api_v1/bookings_router.py
@@ -5,3 +5,26 @@ from core.config import settings
 
 router = APIRouter(prefix=settings.api.v1.bookings, tags=["Booking Services"])
 
+
+@router.get("/list", summary="Получить все вызовы мастера по времени")
+async def get_list_work_bookings(
+
+):
+    """
+    Получает список вызовов матера для каждого пользователя.
+
+    :params None
+    """
+    pass
+
+
+@router.post("/create", summary="Добавить запись по вызову мастера")
+async def create_work_booking(
+) :
+    """
+    Создает вызов мастера по свободной дате и времени и выбранной работы.
+
+    :? Уникальное Название услуги \n
+
+    """
+    pass
